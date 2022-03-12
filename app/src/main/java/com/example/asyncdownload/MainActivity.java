39349f13d74f67f82b52a3a40cc18e78b6578cb0
jainsakshi2395 <sakshi2395@gmail.com>
2022-03-11 17:13:12 -0800

    made some changes

--- app/src/main/java/com/example/asyncdownload/MainActivity.java
+++ app/src/main/java/com/example/asyncdownload/MainActivity.java
@@ -41,27 +41,38 @@ public class MainActivity extends AppCompatActivity {
 
         e1 = (EditText) findViewById(R.id.file1);
         String s1 = e1.getText().toString();
+        if(s1==null){
+            s1 = "https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/45530.pdf";
+        }
         e2 = (EditText) findViewById(R.id.file2);
         String s2 = e2.getText().toString();
+        if(s2== null){
+          s2 = "https://hadoop.apache.org/docs/r1.2.1/hdfs_design.pdf";
+        }
         e3 = (EditText) findViewById(R.id.file3);
         String s3 = e3.getText().toString();
+        if(s3==null){
+            s3 = "https://pages.databricks.com/rs/094-YMS-629/images/LearningSpark2.0.pdf";
+        }
         e4 = (EditText) findViewById(R.id.file4);
         String s4 = e4.getText().toString();
+        if(s4==null){
+            s4 = "https://docs.aws.amazon.com/wellarchitected/latest/machine-learning-lens/wellarchitected-machine-learning-lens.pdf";
+        }
         e5 = (EditText) findViewById(R.id.file5);
         String s5 = e5.getText().toString();
 
+        if(s5 == null){
+            s5= "https://developers.snowflake.com/wp-content/uploads/2020/09/SNO-eBook-7-Reference-Architectures-for-Application-Builders-MachineLearning-DataScience.pdf";
+        }
+
         ArrayList<String> allFiles = new ArrayList<String>();
-   /*     allFiles.add(s1);
+        allFiles.add(s1);
         allFiles.add(s2);
         allFiles.add(s3);
         allFiles.add(s4);
-        allFiles.add(s5);*/
+        allFiles.add(s5);
 
-        allFiles.add("https://static.googleusercontent.com/media/research.google.com/en//pubs/archive/45530.pdf");
-        allFiles.add("https://hadoop.apache.org/docs/r1.2.1/hdfs_design.pdf");
-        allFiles.add("https://pages.databricks.com/rs/094-YMS-629/images/LearningSpark2.0.pdf");
-        allFiles.add("https://docs.aws.amazon.com/wellarchitected/latest/machine-learning-lens/wellarchitected-machine-learning-lens.pdf");
-        allFiles.add("https://developers.snowflake.com/wp-content/uploads/2020/09/SNO-eBook-7-Reference-Architectures-for-Application-Builders-MachineLearning-DataScience.pdf");
 
         Log.e(TAGNAME, "get Files" + allFiles);
 
